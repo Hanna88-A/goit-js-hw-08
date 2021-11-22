71de8833b74a0a30630119aafec67a50e31f501e
Hanna88-A <annamukolaivna88@gmail.com>
2021-11-23 01:39:59 +0200

    Добавляеть первую задачу

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => 
+    `<a class="gallery__item" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"/>
+    </a>`
+    ).join('');
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a',
+    {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
