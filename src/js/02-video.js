47e5529284dc339201254d84b7f1e2c421e5e0ba
Hanna88-A <annamukolaivna88@gmail.com>
2021-11-25 01:25:44 +0200

    Добавляет вторую задачу

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = localStorage.getItem("videoplayer-current-time")
+
+
+player.on("timeupdate", throttle(onPlay, 1000));
+
+function onPlay(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+}
+   
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+
+
+
 
