1d982d7afc137ec2ec077a59586cc0a4e1661cf4
Hanna88-A <annamukolaivna88@gmail.com>
2021-11-27 15:36:34 +0200

    Добавляет задачу №3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = "feedback-form-state";
+let objectFormElements = {};
+
+form.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+populateMessage()
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+    console.log(objectFormElements);
+    
+}
+
+function onTextareaInput(e) {
+    objectFormElements = {
+        mail: form.elements.email.value,
+        message: form.elements.message.value,
+    };
+   
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(objectFormElements));
+}
+
+function populateMessage(e) {
+    const savedobjectFormElements= localStorage.getItem(STORAGE_KEY);
+    const parsedobjectFormElements = JSON.parse(savedobjectFormElements);
+    
+    if (parsedobjectFormElements) {
+        form.elements.email.value = parsedobjectFormElements.mail;
+        form.elements.message.value = parsedobjectFormElements.message;
+    }
+}
 
